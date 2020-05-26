a17eb677d333bea57569a4bd1129e54e9d49d5cc
LiuSaiSai <weiyiliusai@outlook.com>
2020-05-26 17:52:48 +0800

    下载即可用

---
+++ app/src/main/java/com/example/flowlayoutdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.flowlayoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 流式布局；资源来源于网络，回归于网络，如有侵权，请联系：https://blog.csdn.net/liusaisaiV1
+ * PS：自定义 flowlayout；两个都可以；
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
